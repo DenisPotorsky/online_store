3b6805c10f9922ced0f949c32047b2600e8c18d6
DenisPotorsky <denis-s2@yandex.ru>
2024-02-04 09:30:26 +0500

    подготовка к серверу

--- app_store/app_store/settings.py
+++ app_store/app_store/settings.py
@@ -19,15 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7^!33yqr*f+j=e=8%1c$jf)1ixi$wsp16f7n(ep_cd&(fa)g*6'
-# SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-7^!33yqr*f+j=e=8%1c$jf)1ixi$wsp16f7n(ep_cd&(fa)g*6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '192.168.0.2',
     '127.0.0.1',
-    'hwgb.pythonanywhere.com',
+    'whgb.pythonanywhere.com'
+    # 'hwgb.pythonanywhere.com',
 ]
 INTERNAL_IPS = [
     '127.0.0.1',
@@ -87,8 +90,15 @@ WSGI_APPLICATION = 'app_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '<whgb$default>',
+        'USER': '<whgb>',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'whgb.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
